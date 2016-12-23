bc139e93f62fa9af104fb029ddd841cb4d759c61
zmienny <zmiennoimienny@gmail.com>
2016-12-23 16:23:52 +0100

    Adding Math class

---
+++ source/engine/Math.cpp
@@ -0,0 +1,17 @@
+#include "Math.h"
+
+Math::Math()
+{
+    //ctor
+}
+
+Math::~Math()
+{
+    //dtor
+}
+
+Math::PerlinNoise(float a, float b)
+{
+
+
+}
---
+++ source/engine/Math.h
@@ -0,0 +1,17 @@
+#ifndef MATH_H
+#define MATH_H
+
+
+class Math
+{
+    public:
+        Math();
+        virtual ~Math();
+
+        static float PerlinNoise(float a, float b);
+    protected:
+
+    private:
+};
+
+#endif // MATH_H
