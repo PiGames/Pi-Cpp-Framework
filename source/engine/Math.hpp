56fc14245cb9619d7363d7c4b582d45d27f4c26e
zmienny <zmiennoimienny@gmail.com>
2016-12-25 20:28:18 +0100

    Adding perlin noise

--- source/engine/Math.hpp
+++ source/engine/Math.hpp
@@ -1,6 +1,10 @@
 #ifndef MATH_H
 #define MATH_H
 
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <random>
 
 class Math
 {
@@ -8,11 +12,15 @@ class Math
         Math();
         virtual ~Math();
 
-
-        static float PerlinNoise(float a, float b);
+        static double PerlinNoise(double x, double y, double z);
     protected:
 
     private:
+
+
+        static double fade(double t);
+        static double lerp(double t, double a, double b);
+        static double grad(int hash, double x, double y, double z);
 };
 
 #endif // MATH_H
