99d15f6ee12e76b01cdbf720d0f91027057cf72f
mvxxx <yohama8832@o2.pl>
2016-12-28 23:37:58 +0100

    Extended MapManager

--- source/engine/map/MapManager.cpp
+++ source/engine/map/MapManager.cpp
@@ -2,23 +2,45 @@
 
 namespace pi
 {
+
+	sf::Vector2i MapManager::unitWorldSize;
 	Cell* MapManager::surface;
 	std::vector<pi::GameObject> MapManager::objects;
 	bool* MapManager::collisionMap;
 
 
-	void MapManager::createArrays(sf::Vector2i *unitWorldSize)
+	void MapManager::createArrays()
 	{
-		surface = new Cell[unitWorldSize->x*unitWorldSize->y];
-		collisionMap = new bool[unitWorldSize->x*unitWorldSize->y];
+		surface = new Cell[unitWorldSize.x*unitWorldSize.y];
+		collisionMap = new bool[unitWorldSize.x*unitWorldSize.y];
 	}
 
-	void MapManager::Init(sf::Vector2i unitWorldSize)
+	void MapManager::Init(sf::Vector2i uWorldSize)
 	{
-		createArrays(&unitWorldSize);
+		unitWorldSize = uWorldSize;
+		createArrays();
 		CompleteCollisionMapUpdate();
 	}
 
-	void MapManager::CompleteCollisionMapUpdate() { }
+	void MapManager::CompleteCollisionMapUpdate()
+	{
+		for (size_t i = 0; i < unitWorldSize.x*unitWorldSize.x; i++) //to do improve 
+		{
+			collisionMap[i] = surface[i].getComponent<PathfinderAttributes>()->getFlag(PathfinderAttributes::FLAGS::COLLIDABLE);
+		}
+
+		for(auto &var:objects)
+			for (auto &prev : var.getUnitPosition())
+				collisionMap[prev.y*unitWorldSize.x + prev.x] = surface[prev.y*unitWorldSize.x + prev.x].getComponent<PathfinderAttributes>()->getFlag(PathfinderAttributes::FLAGS::COLLIDABLE) && var.getComponent<PathfinderAttributes>()->getFlag(PathfinderAttributes::FLAGS::COLLIDABLE);
+
+	}
+
+	void MapManager::UpdateSingleUnits(GameObject* object)
+	{
+		bool flag = object->getComponent<PathfinderAttributes>()->getFlag(PathfinderAttributes::FLAGS::COLLIDABLE);
+		
+		for (auto& var : object->getUnitPosition())
+			collisionMap[var.y*unitWorldSize.x + var.x] = surface[var.y*unitWorldSize.x + var.x].getComponent<PathfinderAttributes>()->getFlag(PathfinderAttributes::FLAGS::COLLIDABLE) && flag;
+	}
 }
 
--- source/engine/map/MapManager.hpp
+++ source/engine/map/MapManager.hpp
@@ -8,19 +8,26 @@ namespace pi
 	class MapManager
 	{
 		static Cell *surface;
-		static std::vector<pi::GameObject> objects;
+		static std::vector<GameObject> objects;
 
+		//true - collidable unit
+		//false - not collidable unit
 		static bool *collisionMap;
 
-		static void createArrays(sf::Vector2i *unitWorldSize);
+		static void createArrays();
+
+		static sf::Vector2i unitWorldSize;
 
 	public:
 
 		//Initialization (on the begining of game process)
-		static void Init(sf::Vector2i unitWorldSize);
+		static void Init(sf::Vector2i uWorldSize);
 
 		//Updating the whole collision map | Don't overuse this method!
 		static void CompleteCollisionMapUpdate();
+
+		//Updating 1 unit of collision map
+		static void UpdateSingleUnits(GameObject* object);
 	};
 
 }
