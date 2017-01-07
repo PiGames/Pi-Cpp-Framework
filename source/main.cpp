87c23525cdf77a36b51b0b3eed9ff6f1e4c775db
Condzi <conradkubacki@gmail.com>
2017-01-07 21:39:52 +0100

    Clean up main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,19 +1,9 @@
 #include <iostream>
-#include "engine\savesystem\SaveSystem.hpp"
 
 int main()
 {
-	std::cout << "Mars Colony \ncreated by PiGames \n";
-	
-	
-	
-	auto MyCfg = pi::SaveSystem();
+	std::cout << "Mars Colony \nCreated by PiGames\n";
 
-	MyCfg.loadFromFile("data/save_2.txt");
-	MyCfg.addVariable("Kills", "5002");
-	MyCfg.saveToFile("data/save_3.txt");
-	
-	
 	std::cin.get();
 	return 0;
 }
