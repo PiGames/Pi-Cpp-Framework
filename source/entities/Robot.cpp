e4149bcaa7e5dc8de4967bb2e5fc7a2674a87e54
mvxxx <yohama8832@o2.pl>
2017-01-07 22:51:10 +0100

    Changes in Robot

--- source/entities/Robot.cpp
+++ source/entities/Robot.cpp
@@ -3,8 +3,7 @@
 namespace mc
 {
 	Robot::Robot(const std::string & name, const sf::Vector2f & pos, const float& speed, const float& batteryCap)
-		:pi::MovableObject(name,pos,speed), batteryCapacity(batteryCap)
+		:pi::MovableObject(name,pos,speed), batteryCapacity(batteryCap), distance(0), energy(batteryCapacity)
 	{
-		energy = batteryCapacity;
 	}
 }
--- source/entities/Robot.hpp
+++ source/entities/Robot.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "engine/map/MapObject.hpp"
 #include "engine/map/MovableObject.hpp"
 
 namespace mc
@@ -11,7 +10,13 @@ namespace mc
 		Robot(const std::string& name = "<unnamed Robot>", const sf::Vector2f& pos = { 0,0 }, const float& speed = 1.f, const float& batteryCap = 100.f);
 
 	private:
+		//Value which represents battery capacity
 		const float batteryCapacity;
+
+		//Distance covered
+		float distance;
+
+		//The remaining energy
 		float energy;
 	};
 }
