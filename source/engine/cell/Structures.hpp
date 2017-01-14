79893abd4fd664af4fb7c84be8b41925c0f5c794
Condzi <conradkubacki@gmail.com>
2017-01-14 23:15:25 +0100

    Added constexpr_string

---
+++ source/engine/cell/Structures.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace pi
+{
+	struct constexpr_string
+	{
+		const char * str;
+		const size_t size;
+
+		template<size_t count>
+		constexpr constexpr_string(const char (&str)[count]) :
+			str(str),
+			size(count-1) // do not count'\0'
+		{}
+	};
+}
