40433e32fcbcf1f81793a31f65b1627b05e5592b
Condzi <conradkubacki@gmail.com>
2017-01-15 20:01:41 +0100

    Added naive exception handling

--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 
+#include <SFML/Graphics.hpp>
+
 #include "engine/game/Game.hpp"
 #include "states/PlayState.hpp"
-#include <SFML/Graphics.hpp>
 
 int main()
 {
-	pi::Game game({ 1280, 720 }, "Mars Colony", 60u);
+	try
+	{
+		pi::Game game({ 1280, 720 }, "Mars Colony", 60u);
 
-	pi::Logger::log("Mars Colony v" + std::to_string(pi::constants::version::MAJOR) + "." + std::to_string(pi::constants::version::MINOR) + "." + std::to_string(pi::constants::version::RELEASE), pi::Logger::MessageType::Info, pi::Logger::OutputType::Both);
+		pi::Logger::log("Mars Colony v" + std::to_string(pi::constants::version::MAJOR) + "." + std::to_string(pi::constants::version::MINOR) + "." + std::to_string(pi::constants::version::RELEASE), pi::Logger::MessageType::Info, pi::Logger::OutputType::Both);
 
-	game.addState<mc::PlayState>((short)mc::States::Play);
-	game.run();
+		game.addState<mc::PlayState>((short)mc::States::Play);
+		game.run();
+	}
+	catch (std::exception& e)
+	{
+		pi::Logger::log("EXCPTION: " + *e.what(), pi::Logger::MessageType::Error, pi::Logger::OutputType::Both);
+		std::cin.get();
+	}
+
 
 	return 0;
 }
