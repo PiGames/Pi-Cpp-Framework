3d957213646e2f46343e5f4d870cf576db3a56fa
Condzi <conradkubacki@gmail.com>
2017-02-03 18:36:26 +0100

    Moved Structures.hpp file to engine directory

--- source/engine/Config.hpp
+++ source/engine/Config.hpp
@@ -51,15 +51,16 @@ namespace pi
 		namespace mapManager
 		{
 			const std::map<std::string, sf::Vector2i> unitDirections =
-			{ {"NORTH",{0,-1}},
-			  {"NORTH_EAST",{1,-1}},
-			  { "EAST",{1,0}},
-			  { "SOUTH_EAST",{1,1}},
-			  { "SOUTH",{0,1} },
-			  { "SOUTH_WEST",{-1,1}},
-			  { "WEST",{-1,0}},
-			  { "NORT_WEST",{-1,-1}}
-		};
+			{
+				{ "NORTH",		{0,-1}	},
+				{ "NORTH_EAST",	{1,-1}	},
+				{ "EAST",		{1,0}	},
+				{ "SOUTH_EAST",	{1,1}	},
+				{ "SOUTH",		{0,1}	},
+				{ "SOUTH_WEST",	{-1,1}	},
+				{ "WEST",		{-1,0}	},
+				{ "NORT_WEST",	{-1,-1}	}
+			};
 
 			namespace nameDirections
 			{
