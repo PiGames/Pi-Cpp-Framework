10a1ebafd6f139806b16d3cc5391d204f311c9b4
Condzi <conradkubacki@gmail.com>
2017-02-18 10:43:09 +0100

    Added Types.hpp file with examples

---
+++ source/engine/Types.hpp
@@ -0,0 +1,29 @@
+// Created by: Condzi (but everyone will edit it)
+#pragma once
+
+namespace pi
+{
+	////////////////////
+	//	Enumerations  //
+	////////////////////
+
+	enum ExampleEnum_t
+	{
+		EXAMPLE_NAME
+	};
+
+
+	////////////////////
+	//	 Structures   //
+	////////////////////
+
+	struct MyStruct_t final
+	{
+		float someVariable;
+
+		void someMethod()
+		{
+			// Some code - if method is larger than 5 lines then make Types.cpp and there define it
+		}
+	};
+}
